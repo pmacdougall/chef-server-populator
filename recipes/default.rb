852bd9811a78a2cce14c56bcac6bd01da807a6ab
Paul MacDougall <pmacdougall@newcontext.com>
2014-07-14 16:19:06 -0700

    adding a users recipe to generate users along with clients

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,7 @@ if(Chef::Config[:solo])
   include_recipe 'chef-server-populator::solo'
 else
   include_recipe 'chef-server-populator::client'
+  include_recipe 'chef-server-populator::user'
 
   unless(node[:chef_server_populator][:chef_server].empty?)
     include_recipe 'chef-server-populator::configurator'
